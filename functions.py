88f2f44841e121b8aa71b9699f3fddb48d83464c
Gustavo Sousa <gustavosousabrazil@gmail.com>
2024-04-08 18:01:10 -0300

    v4

--- functions.py
+++ functions.py
@@ -1,3 +1,12 @@
 def funcao_soma(a: int, b: int) -> int:
+    """SOma dois números
+
+    Args:
+        a (int): inteiro 1
+        b (int): interio 2
+
+    Returns:
+        int: soma
+    """
 
     return a + b
